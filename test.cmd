a3ff2a87c0d00111cd62773bcd6983e153c4e648
p0deje <p0deje@gmail.com>
2022-10-30 20:07:00 -0700

    Add cmd script for testing

---
+++ test.cmd
@@ -0,0 +1,14 @@
+cd examples/gem
+
+:: rb_library
+bazel build :gem || exit /b
+
+:: rb_binary
+bazel run lib/gem:print-version || exit /b
+bazel run :rubocop || exit /b
+
+:: rb_test
+bazel test spec:all || exit /b
+
+:: rb_gem
+bazel build :example || exit /b
