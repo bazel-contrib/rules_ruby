14a09bd380f03c8e81a6dc4a0edcf9ae00098629
Alex Rodionov <p0deje@gmail.com>
2023-11-23 18:27:06 -0800

    docs: update installation instructions

--- .github/workflows/release_prep.sh
+++ .github/workflows/release_prep.sh
@@ -30,6 +30,17 @@ Paste this snippet into your \`WORKSPACE.bazel\` file:
 
 \`\`\`starlark
 load("@bazel_tools//tools/build_defs/repo:http.bzl", "http_archive")
+
+# Skylib is a dependency, remove if you already have it.
+http_archive(
+    name = "bazel_skylib",
+    sha256 = "74d544d96f4a5bb630d465ca8bbcfe231e3594e5aae57e1edbf17a6eb3ca2506",
+    urls = [
+        "https://mirror.bazel.build/github.com/bazelbuild/bazel-skylib/releases/download/1.3.0/bazel-skylib-1.3.0.tar.gz",
+        "https://github.com/bazelbuild/bazel-skylib/releases/download/1.3.0/bazel-skylib-1.3.0.tar.gz",
+    ],
+)
+
 http_archive(
     name = "rules_ruby",
     sha256 = "${SHA}",
