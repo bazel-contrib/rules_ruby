8ff99106acbb5a0b3320a43522f8760e0ab445db
Alex Rodionov <p0deje@gmail.com>
2025-01-02 12:48:04 -0800

    feat: support gems with native extensions

---
+++ examples/native_ext/ext/native_ext/native_ext.h
@@ -0,0 +1,6 @@
+#ifndef NATIVE_EXT_H
+#define NATIVE_EXT_H 1
+
+#include "ruby.h"
+
+#endif /* NATIVE_EXT_H */
---
+++ examples/native_ext/lib/native_ext/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module NativeExt
+  VERSION = "0.1.0"
+end
