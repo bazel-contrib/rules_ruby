e38768861c8abe8bfe5ee5e7a52b28ca892febfc
Alex Rodionov <p0deje@gmail.com>
2025-01-23 13:57:45 -0800

    chore: cleanup native_ext files

--- examples/native_ext/lib/native_ext.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-require_relative "native_ext/version"
-require_relative "native_ext/native_ext"
-
-module NativeExt
-  class Error < StandardError; end
-  # Your code goes here...
-end
