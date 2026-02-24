09f5ba47f045f272790f97b8fbdf3b611be78551
Alex Rodionov <p0deje@gmail.com>
2026-02-24 10:48:29 -0800

    feat!: drop support for bazel 6
    It's no longer maintained since December 2025 https://bazel.build/release

--- .github/workflows/release_prep.sh
+++ .github/workflows/release_prep.sh
@@ -23,45 +23,11 @@ tar --create --auto-compress \
 
 # The stdout of this program will be used as the top of the release notes for this release.
 cat << EOF
-## Using Bzlmod with Bazel 7-8
+## Using Bzlmod
 
 1. Add to your \`MODULE.bazel\` file:
 
 \`\`\`starlark
 bazel_dep(name = "rules_ruby", version = "${TAG:1}")
-\`\`\`
-
-## Using Bzlmod with Bazel 6
-
-1. Enable with \`common --enable_bzlmod\` in \`.bazelrc\`.
-2. Add to your \`MODULE.bazel\` file:
-
-\`\`\`starlark
-bazel_dep(name = "rules_ruby", version = "${TAG:1}")
-\`\`\`
-
-## Using WORKSPACE
-
-Paste this snippet into your \`WORKSPACE.bazel\` file:
-
-\`\`\`starlark
-load("@bazel_tools//tools/build_defs/repo:http.bzl", "http_archive")
-
-# Skylib is a dependency, remove if you already have it.
-http_archive(
-    name = "bazel_skylib",
-    sha256 = "74d544d96f4a5bb630d465ca8bbcfe231e3594e5aae57e1edbf17a6eb3ca2506",
-    urls = [
-        "https://mirror.bazel.build/github.com/bazelbuild/bazel-skylib/releases/download/1.3.0/bazel-skylib-1.3.0.tar.gz",
-        "https://github.com/bazelbuild/bazel-skylib/releases/download/1.3.0/bazel-skylib-1.3.0.tar.gz",
-    ],
-)
-
-http_archive(
-    name = "rules_ruby",
-    sha256 = "${SHA}",
-    strip_prefix = "${PREFIX}",
-    url = "https://github.com/bazel-contrib/rules_ruby/releases/download/${TAG}/${ARCHIVE}",
-)
 \`\`\`
 EOF
