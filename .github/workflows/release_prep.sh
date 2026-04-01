f1c9240b41f2b034d6a553205dd5c8ddb96a3d0d
Alex Eagle <alex@aspect.dev>
2026-04-01 11:23:40 -0700

    chore: enable immutable releases and publish-to-bcr

--- .github/workflows/release_prep.sh
+++ .github/workflows/release_prep.sh
@@ -2,16 +2,15 @@
 
 set -o errexit -o nounset -o pipefail
 
-# Set by GH actions, see
-# https://docs.github.com/en/actions/learn-github-actions/environment-variables#default-environment-variables
-TAG=${GITHUB_REF_NAME}
+# Argument provided by reusable workflow caller, see
+# https://github.com/bazel-contrib/.github/blob/d197a6427c5435ac22e56e33340dff912bc9334e/.github/workflows/release_ruleset.yaml#L72
+TAG=$1
 # The prefix is chosen to match what GitHub generates for source archives
 PREFIX="rules_ruby-${TAG:1}"
 ARCHIVE="rules_ruby-$TAG.tar.gz"
 
 # NB: configuration for 'git archive' is in /.gitattributes
-git archive --format=tar --prefix=${PREFIX}/ ${TAG} | gzip > $ARCHIVE
-SHA=$(shasum -a 256 $ARCHIVE | awk '{print $1}')
+git archive --format=tar --prefix=${PREFIX}/ ${TAG} | gzip > "$ARCHIVE"
 
 # Add generated API docs to the release, see https://github.com/bazelbuild/bazel-central-registry/issues/5593
 docs="$(mktemp -d)"; targets="$(mktemp)"
